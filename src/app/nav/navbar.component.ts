af59f7a687eddbb50d15803d06980f755696581f
Anshu <anshukanishka@gmail.com>
2019-05-01 13:19:35 +0530

    Adding Search filter in navbar

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import { IUsers } from '../users/shared/user.model';
+import { UserService } from '../users/shared/user.service';
 
 @Component({
     // tslint:disable-next-line:component-selector
@@ -6,5 +8,14 @@ import {Component} from '@angular/core';
     templateUrl: './navbar.component.html'
 })
 export class NavBarComponent {
+    searchTerm = '';
+    foundUsers: IUsers;
+    constructor(private userService: UserService) { }
 
+    searchUsers(searchTerm) {
+        this.userService.getUsers(searchTerm).subscribe(user => {
+            console.log(user);
+            this.foundUsers = user;
+        });
+    }
 }
--- src/app/users/users-list.components.ts
+++ src/app/users/users-list.components.ts
@@ -8,10 +8,10 @@ import { IUsers, IUser } from './shared/user.model';
     selector: 'users-list',
     template: `
     <div>
-    <h5>Total Results : {{users.total_count}}</h5>
+    <h5>Total Results : {{totalCount}}</h5>
     <hr>
     <div class="row">
-            <div *ngFor="let user of users.items" class="col-5">
+            <div *ngFor="let user of users" class="col-5">
                 <user-thumbnail [user]="user"></user-thumbnail>
             </div>
         </div>
@@ -19,13 +19,15 @@ import { IUsers, IUser } from './shared/user.model';
     `
 })
 export class UsersListComponent implements OnInit {
-    users: IUsers;
+    users: IUser[];
+    totalCount: number;
     constructor(private userService: UserService) { }
 
     ngOnInit() {
         const searchParam = 'henry';
         this.userService.getUsers(searchParam).subscribe((res: IUsers) => {
-            this.users = res;
+            this.users = res.items;
+            this.totalCount = res.total_count;
         });
     }
 }
